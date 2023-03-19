a309b1691f6073e5c2fad2bd325e1d1506a8e104
LukeAnger <anger.luke@gmail.com>
2023-03-19 16:03:58 -0400

    updated repo name handling and option TS/eslint v1.1.3

--- index.js
+++ index.js
@@ -1,10 +1,57 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const { prompt } = require('inquirer');
 const projectName = process.argv[2] || 'my-mern-app';
 
-execSync(`git clone https://github.com/LukeAnger/create-mern-stack-app.git ${projectName}`);
-execSync(`cd ${projectName} && npm install && npm start`);
+prompt({
+    type: 'input',
+    name: 'projectName',
+    message: "Please specify the project directory (e.g. 'my-mern-app')",
+  })
+  .then((answers) => {
+    const projectName = answers.projectName;
+    // rest of your code goes here
+  });
 
-// Rename the directory to the user-specified name
-execSync(`mv ${projectName} ${process.cwd()}/${projectName}`);
+
+const questions = [
+  {
+    type: 'input',
+    name: 'projectName',
+    message: "Please specify the project directory (e.g. 'my-mern-app')",
+  },
+  {
+    type: 'confirm',
+    name: 'installTypescript',
+    message: 'Do you want to install TypeScript?',
+    default: true,
+  },
+  {
+    type: 'confirm',
+    name: 'installEslint',
+    message: 'Do you want to install ESLint?',
+    default: true,
+  },
+];
+
+prompt(questions).then(answers => {
+  const { projectName, installTypescript, installEslint } = answers;
+
+  const projectName = answers.projectName;
+  const gitCloneCmd = `git clone https://github.com/LukeAnger/create-mern-stack-app.git ${projectName}`;
+  const npmInstallCmd = `cd ${projectName} && npm install`;
+
+  if (installTypescript) {
+    npmInstallCmd += ' typescript';
+  }
+
+  if (installEslint) {
+    npmInstallCmd += ' eslint';
+  }
+
+  execSync(`${gitCloneCmd} && ${npmInstallCmd} && npm start`);
+
+  // Rename the directory to the user-specified name
+  execSync(`mv ${projectName} ${process.cwd()}/${projectName}`);
+});
